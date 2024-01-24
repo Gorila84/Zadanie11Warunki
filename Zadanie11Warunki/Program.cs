b67a696a7d46c3c58237107556ac25d763d22eff
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 21:45:55 +0100

    Add project files.

---
+++ Zadanie11Warunki/Program.cs
@@ -0,0 +1,15 @@
+﻿int numerciGrade;
+string[] grades = new string[] {
+    "Niedostateczny",
+    "Dopuszczający",
+    "Dostateczny",
+    "Dobry",
+    "Bardzo dobry",
+    "Celujący"};
+
+
+Console.WriteLine("Podaj ocenę (1,2,3,4,5,6): ");
+var enteredGrade = Console.ReadLine();
+Int32.TryParse(enteredGrade, out numerciGrade);
+
+Console.WriteLine(grades[numerciGrade -1]);
