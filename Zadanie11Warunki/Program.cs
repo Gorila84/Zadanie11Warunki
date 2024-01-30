6ad16b8284b50db453c6cfc59c2288ba3b8be3d6
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-30 22:22:10 +0100

    Changing the code on the switch. I leave the commented code as a reminder

--- Zadanie11Warunki/Program.cs
+++ Zadanie11Warunki/Program.cs
@@ -9,7 +9,32 @@ string[] grades = new string[] {
 
 
 Console.WriteLine("Podaj ocenę (1,2,3,4,5,6): ");
-var enteredGrade = Console.ReadLine();
-Int32.TryParse(enteredGrade, out numerciGrade);
+var enteredGrade = Console.ReadKey();
+//Int32.TryParse(enteredGrade, out numerciGrade);
 
-Console.WriteLine(grades[numerciGrade -1]);
+//Console.WriteLine(grades[numerciGrade -1]);
+
+switch (enteredGrade.KeyChar)
+{
+    case '1':
+        Console.WriteLine("\n" + grades[0]);
+        break;
+    case '2':
+        Console.WriteLine("\n" + grades[1]);
+        break;
+    case '3':
+        Console.WriteLine("\n" + grades[2]);
+        break;
+    case '4':
+        Console.WriteLine("\n" + grades[3]);
+        break;
+    case '5':
+        Console.WriteLine("\n" + grades[4]);
+        break;
+    case '6':
+        Console.WriteLine("\n" + grades[5]);
+        break;
+    default:
+        Console.WriteLine("Wpisałeś niepoprawny numer oceny.");
+        break;
+}
